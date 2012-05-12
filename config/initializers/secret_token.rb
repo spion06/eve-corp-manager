a2716d5848412703eb7c57352437e38d0dbc4100
Kyle McGovern <spion@mint>
2012-05-12 18:18:43 -0500

    added rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EveCorpManager::Application.config.secret_token = '403d38c19bea737ad4488c1612fc9d17216b7984bbb935f6ebb1fb367b1527c19acfcac58bf6225501afac9293fb39ab6c85be302f4e939e4f5bcb874fb016dd'
